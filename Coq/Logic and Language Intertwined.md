d604990af6daeff4687912892825e02b3a63fbe3
sho <singhshobhit2000@gmail.com>
2024-12-25 18:16:29 +0530

    clean up

--- Coq/Logic and Language Intertwined.md
+++
@@ -1,6 +0,0 @@
-## [**Logic and Language Intertwined**](https://cs.uwaterloo.ca/~plragde/flaneries/LACI/) 					
-
-##### by Prabhakar Ragde
-
-
-
--- Coq/first.v
+++
@@ -1,7 +0,0 @@
-Fixpoint exp (base power : nat) : nat :=
-  match power with
-  | O => S O
-  | S p => mult base (exp base p)
-  end.
-
-Compute exp 10 6.
